288f15613f0b8e477e1867835057f432affbadf9
B-1 <Yasser.Bishesari@gmail.com>
2025-12-09 01:42:09 +0330

    Home

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
-/*
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-*/
-
-Volt::route('/', 'welcome');
+Volt::route('/', 'welcome')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
